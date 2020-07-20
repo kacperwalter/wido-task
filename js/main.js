1894c5448b9ab3234a75a1d4f822ef2bff190386
Kacper Walter <kacwal@poczta.fm>
2020-07-20 20:29:38 +0200

    added page-title

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.navigation');
+const pageTitle = document.querySelector('.page-title')
 
 const handleClick = () => {
     hamburger.classList.toggle('hamburger--active');
     navigation.classList.toggle('navigation--active');
+    pageTitle.classList.toggle('page-title--active');
 }
 
 hamburger.addEventListener('click', handleClick);
